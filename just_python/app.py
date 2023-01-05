272ca508f50793e794460bb4bb732775cf518b99
Renan Odilon Oliveira <renanodilon@gmail.com>
2023-01-05 11:09:48 +0000

    exemplos de api da live Python para web APIs na LinuxTips by Bruno Rocha

---
+++ just_python/app.py
@@ -0,0 +1,14 @@
+import json
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+dados = {"canal":"LinuxTips", "msg":"vaaaaii!!!"}
+
+class API(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200) # Ok
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(dados).encode("utf-8"))
+
+server = HTTPServer(("0.0.0.0", 8000), API)
+server.serve_forever()
