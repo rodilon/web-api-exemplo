aa4086a17885c3bcfc25eea867767a8c0a328367
Renan Odilon Oliveira <renanodilon@gmail.com>
2023-01-05 11:08:48 +0000

    exemplos de api da live Python para web APIs na LinuxTips by Bruno Rocha

---
+++ fastapi_example/api.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class LinuxTips(BaseModel):
+    canal: str
+    msg: str
+    dia: int
+
+@app.get("/", response_model=LinuxTips)
+async def linuxtips():
+    return LinuxTips(
+        canal="LinuxTips",
+        msg="VAIIII",
+        dia=25
+    )
